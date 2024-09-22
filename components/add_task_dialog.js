f9e5e4dd4643f24d2956f7edc2a7336d7e6c21ae
AkshatGMKit <akshat@gkmit.co>
2024-09-23 02:07:22 +0530

    refactor: Removed unnecessary code

--- components/add_task_dialog.js
+++
@@ -1,7 +0,0 @@
-import { createElement } from "../helpers/helpers.js";
-import * as style from "../styles/style.js";
-
-export const addUpdateTaskDialogBox = createElement({
-	type: "div",
-	styles: style.taskDialogStyle,
-});
